77519c043dc3db112bc6c0e9b34ba7aa791ffc55
Quy Tran <quytrant@nashtechglobal.com>
2024-06-04 13:10:06 +0700

    Add Order service

---
+++ MSA.OrderService/Program.cs
@@ -0,0 +1,39 @@
+using MSA.OrderService.Domain;
+using MSA.OrderService.Infrastructure.Data;
+using MSA.Common.Contracts.Settings;
+using MSA.Common.PostgresMassTransit.PostgresDB;
+
+var builder = WebApplication.CreateBuilder(args);
+
+PostgresDBSetting serviceSetting = builder.Configuration.GetSection(nameof(PostgresDBSetting)).Get<PostgresDBSetting>();
+
+
+// Add services to the container.
+builder.Services
+    .AddPostgres<MainDbContext>()
+    .AddPostgresRepositories<MainDbContext, Order>()
+    .AddPostgresUnitofWork<MainDbContext>();
+
+builder.Services.AddControllers(opt => {
+    opt.SuppressAsyncSuffixInActionNames = false;
+});
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
